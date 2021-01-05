7d9a8071a5290d737fc728e9f02beb0bc2921b65
Nasser <abdelnasser.kh10@gmail.com>
2021-01-05 13:16:20 +0300

    use withRouter HOC with menuItem component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component'
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+const ApplePage = () => (
+  <h1> Apple </h1>
+)
+
 
 function App() {
   return (
     <div>
-     <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/apple' component={ApplePage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,30 +12,35 @@ class Directory extends React.Component {
         imageUrl: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp-spacegray-select-202011?wid=892&hei=820&&qlt=80&.v=1603406905000',
         id: 1,
         size: '',
+        linkUrl: 'apple'
       },
       {
         title: 'Dell',
         imageUrl: 'https://i.dell.com/sites/csimages/Video_Imagery/all/latitude-9510-olympic.jpg',
         id: 2,
         size: '',
+        linkUrl: ''
       },
       {
         title: 'HP',
         imageUrl: 'https://media.flixfacts.com/inpage/hp/pavilion/mauve_images/big4.jpg',
         id: 3,
         size: '',
+        linkUrl: ''
       },
       {
         title: 'Toshiba',
         imageUrl: 'https://cdn.vatanbilgisayar.com/Upload/PRODUCT/toshiba/thumb/v2-74418_large.jpg',
         id: 4,
         size: 'large',
+        linkUrl: ''
       },
       {
         title: 'Asus',
         imageUrl: 'https://static.bhphoto.com/images/images2500x2500/1572345160_1508643.jpg',
         id: 5,
         size: 'large',
+        linkUrl: ''
       }]
     }
   }
@@ -43,8 +48,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({title, imageUrl, id, size}) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({id, ...otherSectionProps}) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     )
