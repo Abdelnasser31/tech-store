b0de6df6ca9f31371c0d9fd783bf56e8fe2e4886
Nasser <abdelnasser.kh10@gmail.com>
2021-01-09 18:38:13 +0300

    create sign in page with sign in component and sign in with google

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/firebase.utils';
 
 import './header.styles.scss';
 
-const Header = () => (
+const Header = ({currentUser}) => (
   <div className='header'>
     <Link to='/' className='logo-container'>
       <span className='logo'>Logo</span>
@@ -12,6 +13,9 @@ const Header = () => (
     <div className='options'>
       <Link className='option' to='/shop'>Shop</Link>
       <Link className='option' to='/contact'>Contact</Link>
+      { currentUser ? 
+      <div className='option' onClick={() => auth.signOut()}>Sign out</div> :
+      <Link to='/signin' className='option'>Sign in</Link>}
     </div>
   </div>
 )
